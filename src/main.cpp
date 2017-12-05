1d714e36f2617b291a7ef1d0524c599e74d95e97
crisem <crisem@gmail.com>
2017-12-05 20:46:25 +0800

    Changes To Merkle Genesis Blocks

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0x14dc23f54de47df797172d66531e7a115b782e65f69dda7bf5a4e98fac0ae086");
+uint256 hashGenesisBlock("0xecd479b40cb89248cb7b59f94c9892f5738b7311e2d540ad631b9d0c4c802519");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -2008,10 +2008,10 @@ bool LoadBlockIndex(bool fAllowNew)
     
 	// Genesis block:
 	/*
-	block.nTime = 1399574511 
-	block.nNonce = 2086299649 
-	block.GetHash = 14dc23f54de47df797172d66531e7a115b782e65f69dda7bf5a4e98fac0ae086
-	CBlock(hash=14dc23f54de47df79717, PoW=00000aeb1f05fbf0b2fa, ver=1, 			hashPrevBlock=00000000000000000000, hashMerkleRoot=ef7e256a2c, 			nTime=1399574511, nBits=1e0ffff0, nNonce=2086299649, vtx=1)
+	block.nTime = 1512467905 
+	block.nNonce = 2086383649 
+	block.GetHash = ecd479b40cb89248cb7b59f94c9892f5738b7311e2d540ad631b9d0c4c802519
+	CBlock(hash=14dc23f54de47df79717, PoW=00000aeb1f05fbf0b2fa, ver=1, 			hashPrevBlock=00000000000000000000, hashMerkleRoot=ef7e256a2c, 			nTime=1399574511, nBits=1e0ffff0, nNonce=2086383649, vtx=1)
   	CTransaction(hash=ef7e256a2c, ver=1, vin.size=1, vout.size=1, nLockTime=0)
     	CTxIn(COutPoint(0000000000, -1), coinbase 				04ffff001d010414466972737420646179206f662050726f6a656374)
     	CTxOut(error)
@@ -2032,7 +2032,7 @@ bool LoadBlockIndex(bool fAllowNew)
         block.nVersion = 1;
         block.nTime    = 1512467905;
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 2086299649;
+        block.nNonce   = 2086383649;
 
         if (fTestNet)
         {
@@ -2044,10 +2044,10 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0xef7e256a2cf2d38576225af2775a1e8160928a0c78526ab3615615d1ff16870e"));
+        assert(block.hashMerkleRoot == uint256("0x4afc0aa1b56f616b70905abb08bd99302b021d9e23f0f4adda5ee6ba1cb0403a"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (true && block.GetHash() != hashGenesisBlock)
+        if (false && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
