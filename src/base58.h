7bb3f32e66666ac02c924d877014198110b11906
crisem <crisem@gmail.com>
2017-12-05 18:02:51 +0800

    Settings Changes for CrisMCoin

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 19, // addresses start with L
+        PUBKEY_ADDRESS = 11, // addresses start with C (based in https://en.bitcoin.it/wiki/Base58Check_encoding)
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #LearnCoinTEST3\r");
-            Send(hSocket, "WHO #LearnCoinTEST3\r");
+            Send(hSocket, "JOIN #CrisMCoinTEST3\r");
+            Send(hSocket, "WHO #CrisMCoinTEST3\r");
         } else {
-            // randomly join #LearnCoin00-#LearnCoin99
+            // randomly join #CrisMCoin00-#CrisMCoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #LearnCoin%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #LearnCoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #CrisMCoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #CrisMCoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
